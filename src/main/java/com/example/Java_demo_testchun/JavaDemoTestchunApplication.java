79a925e18a2a6ae4ace3425cc7237a2cff0b9e43
huichun17 <monday20230220@gmail.com>
2023-03-20 12:05:03 +0800

    chun Commmit

---
+++ src/main/java/com/example/Java_demo_testchun/JavaDemoTestchunApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_demo_testchun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTestchunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTestchunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Java_demo_testchun/JavaDemoTestchunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_demo_testchun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoTestchunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
